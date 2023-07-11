4f88ab63dce4260fb92a9a0d368a039dda7959b2
Дмитрий Кефирский <kefirskiy@gmail.com>
2023-07-11 13:03:51 +0400

    Добавьте файлы проекта.

---
+++ Food/Context/AplicationContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Food.Model; 
+namespace Food.Context
+{
+    public class AplicationContext : DbContext
+    {
+        public DbSet<Foods>? Food { get; set; } = null;
+
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=usersdb;Username=postgres;Password=djdj15929");
+        }
+    }
+}
